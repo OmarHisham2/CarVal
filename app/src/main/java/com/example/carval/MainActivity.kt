327e9eb2e53a3f765f3caa4bf873ea5d04cc6ec6
Omar Hisham <Omar.Hisham.Gouda@outlook.com>
2023-05-15 23:21:50 +0300

    Added 'Purchase Car' screen

--- app/src/main/java/com/example/carval/MainActivity.kt
+++ app/src/main/java/com/example/carval/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.carval
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clickListener()
+    }
+
+    private fun clickListener()
+    {
+        val pCar = findViewById<MaterialCardView>(R.id.purchase_button)
+        val sCar = findViewById<MaterialCardView>(R.id.sell_button)
+
+        pCar.setOnClickListener {
+            openPurchaseActivity()
+        }
+
+        sCar.setOnClickListener {
+            openSellActivity()
+        }
+    }
+
+    private fun openPurchaseActivity()
+    {
+        startActivity(Intent(this@MainActivity,purchaseCar::class.java))
+    }
+    private fun openSellActivity()
+    {
+        startActivity(Intent(this@MainActivity,sellCar::class.java))
+
     }
 }
---
+++ app/src/main/java/com/example/carval/purchaseCar.kt
@@ -0,0 +1,71 @@
+package com.example.carval
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class purchaseCar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase_car)
+
+        val transmissions = resources.getStringArray(R.array.Transmission)
+        val transmission_dropdownmenu = findViewById<Spinner>(R.id.transmission_dropdown)
+
+
+
+
+        // Spinner choices and text color //
+        val spinnerAdapter = object :
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, transmissions) {
+
+            override fun isEnabled(position: Int): Boolean {
+                // Disable the first item from Spinner
+                // First item will be used for hint
+                return position != 0
+            }
+
+            override fun getDropDownView(
+                position: Int,
+                convertView: View?,
+                parent: ViewGroup
+            ): View {
+                val view: TextView =
+                    super.getDropDownView(position, convertView, parent) as TextView
+                //set the color of first item in the drop down list to gray
+                if (position == 0) {
+                    view.setTextColor(Color.GRAY)
+                } else {
+
+                    view.setTextColor(Color.BLACK)
+                }
+                return view
+            }
+        }
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        transmission_dropdownmenu.adapter = spinnerAdapter
+
+        transmission_dropdownmenu.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val value = parent!!.getItemAtPosition(position).toString()
+                    (view as TextView).setTextColor(Color.WHITE)
+            }
+        }
+    }
+
+
+}
